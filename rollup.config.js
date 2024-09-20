6101ced01c4b2918e4ddd76e90acdca1c36cd90e
Doron Zavelevsky <zavelevsky@gmail.com>
2024-09-20 22:47:08 +0100

    fixed rollup config handling of external deps

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json' assert { type: 'json' };
 
 export default {
   input: {
@@ -34,5 +35,8 @@ export default {
     typescript(),
     terser(), // Minifies the bundle
   ],
-  external: [], // Add any external dependencies here
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
 };
