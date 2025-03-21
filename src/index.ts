44516d015a09d20701fc3c30d8a509fc215af6f2
Doron Zavelevsky <zavelevsky@gmail.com>
2025-03-21 17:46:26 +0000

    fixed exports

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export * as chainCache from './chain-cache';
 export * as tradeMatcher from './trade-matcher';
 export * as utils from './utils';
 export * as strategyManagement from './strategy-management';
+export * as adapters from './adapters';
 export * from './common/types';
