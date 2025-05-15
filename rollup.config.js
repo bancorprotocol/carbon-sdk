4a54fc5acd3233d5f2c8e7a6e314e4b2bae65e50
Grandschtroumpf <francois.guezengar@hotmail.fr>
2025-05-15 12:00:30 +0200

    enforce node 22 & yarn 1.22

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
-import pkg from './package.json' assert { type: 'json' };
+import pkg from './package.json' with { type: 'json' };
 
 export default {
   input: {
