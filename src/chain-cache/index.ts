2ac02cbe0af7d851b91be20c69aea08d99603944
Doron Zavelevsky <zavelevsky@gmail.com>
2025-06-12 23:17:30 +0300

    avoid fetching on cache miss

--- src/chain-cache/index.ts
+++ src/chain-cache/index.ts
@@ -43,6 +43,5 @@ export const initSyncedCache = (
     msToWaitBetweenSyncs,
     chunkSize
   );
-  cache.setCacheMissHandler(syncer.syncPairData.bind(syncer));
   return { cache, startDataSync: syncer.startDataSync.bind(syncer) };
 };
